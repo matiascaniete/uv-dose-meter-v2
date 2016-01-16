9c6bc2e373719792e5ce2af41eab50285ac95035
Matias on Ubuntu <matiascaniete@gmail.com>
2016-01-16 09:54:33 +0100

    Imp: Envio de varios valores a la vez

--- transmitter-uno/transmitter-uno.ino
+++ transmitter-uno/transmitter-uno.ino
@@ -1,35 +1,35 @@
 #include <VirtualWire.h>
 
 /*
- MP8511 UV Sensor Read Example
- Date: January 15th, 2014
- License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
+  MP8511 UV Sensor Read Example
+  Date: January 15th, 2014
+  License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
 
- The MP8511 UV Sensor outputs an analog signal in relation to the amount of UV light it detects.
+  The MP8511 UV Sensor outputs an analog signal in relation to the amount of UV light it detects.
 
- Connect the following MP8511 breakout board to Arduino:
- 3.3V = 3.3V
- OUT = A0
- GND = GND
- EN = 3.3V
- 3.3V = A1
- These last two connections are a little different. Connect the EN pin on the breakout to 3.3V on the breakout.
- This will enable the output. Also connect the 3.3V pin of the breakout to Arduino pin 1.
+  Connect the following MP8511 breakout board to Arduino:
+  3.3V = 3.3V
+  OUT = A0
+  GND = GND
+  EN = 3.3V
+  3.3V = A1
+  These last two connections are a little different. Connect the EN pin on the breakout to 3.3V on the breakout.
+  This will enable the output. Also connect the 3.3V pin of the breakout to Arduino pin 1.
 
- This example uses a neat trick. Analog to digital conversions rely completely on VCC. We assume
- this is 5V but if the board is powered from USB this may be as high as 5.25V or as low as 4.75V:
- http://en.wikipedia.org/wiki/USB#Power Because of this unknown window it makes the ADC fairly inaccurate
- in most cases. To fix this, we use the very accurate onboard 3.3V reference (accurate within 1%). So by doing an
- ADC on the 3.3V pin (A1) and then comparing this against the reading from the sensor we can extrapolate
- a true-to-life reading no matter what VIN is (as long as it's above 3.4V).
+  This example uses a neat trick. Analog to digital conversions rely completely on VCC. We assume
+  this is 5V but if the board is powered from USB this may be as high as 5.25V or as low as 4.75V:
+  http://en.wikipedia.org/wiki/USB#Power Because of this unknown window it makes the ADC fairly inaccurate
+  in most cases. To fix this, we use the very accurate onboard 3.3V reference (accurate within 1%). So by doing an
+  ADC on the 3.3V pin (A1) and then comparing this against the reading from the sensor we can extrapolate
+  a true-to-life reading no matter what VIN is (as long as it's above 3.4V).
 
 
- This sensor detects 280-390nm light most effectively. This is categorized as part of the UVB (burning rays)
- spectrum and most of the UVA (tanning rays) spectrum.
+  This sensor detects 280-390nm light most effectively. This is categorized as part of the UVB (burning rays)
+  spectrum and most of the UVA (tanning rays) spectrum.
 
- There's lots of good UV radiation reading out there:
- http://www.ccohs.ca/oshanswers/phys_agents/ultravioletradiation.html
- https://www.iuva.org/uv-faqs
+  There's lots of good UV radiation reading out there:
+  http://www.ccohs.ca/oshanswers/phys_agents/ultravioletradiation.html
+  https://www.iuva.org/uv-faqs
 
 */
 
@@ -40,9 +40,13 @@ int REF_3V3 = A1; //3.3V power on the Arduino board
 const int ledPin = 13;
 const int txPin = 12;
 
+char Sensor1CharMsg[21];// The string that we are going to send trought rf
 
-void setup()
-{
+byte counter = 0;
+int batteryLevel = 0;
+int control = 0;
+
+void setup() {
   // Initialise the IO and ISR
   vw_set_tx_pin(txPin);
   pinMode(ledPin, OUTPUT);
@@ -52,30 +56,30 @@ void setup()
   pinMode(REF_3V3, INPUT);
 }
 
-String value;
-
-void loop()
-{
-  char msg[4];
-
+void loop() {
   unsigned int uvLevel = analogRead(UVOUT);
   unsigned int refLevel = analogRead(REF_3V3);
 
+  counter++;
+
   //Use the 3.3V power pin as a reference to get a very accurate output value from sensor
-  float outputVoltage = (3.3 / refLevel) * uvLevel;
-  value = String( int ( mapfloat(outputVoltage, 0.97, 3.3, 0, 100)) );
-  value = String( int (1023 * uvLevel / refLevel) );
-  value = String( int ( mapfloat( uvLevel, 0, refLevel, 0, 1023 )));
+  int value =  int ( mapfloat( uvLevel, 0, refLevel, 0, 1023 ));
 
-  for (int i = 0; i < value.length(); i++) {
-    msg[i] = value[i];
-  }
+  sprintf(Sensor1CharMsg, "%d,%d,%d,%d,", value, counter, batteryLevel, control);
 
-  digitalWrite(ledPin, HIGH); // Flash a light to show transmitting
-  vw_send((uint8_t *)msg, value.length());
+  // Turn on a light to show transmitting
+  vw_send((uint8_t *)Sensor1CharMsg, strlen(Sensor1CharMsg));
   vw_wait_tx(); // Wait until the whole message is gone
-  digitalWrite(ledPin, LOW);
+  // Turn off a light after transmission
   delay(100);
+
+  /*
+    digitalWrite(ledPin, HIGH); // Flash a light to show transmitting
+    vw_send((uint8_t *)msg, value.length());
+    vw_wait_tx(); // Wait until the whole message is gone
+    digitalWrite(ledPin, LOW);
+    delay(100);
+  */
 }
 
 
